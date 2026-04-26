5717a11fc3ab075328d9f33ce6eee0838d1d0e03
Axel A.L <unknownkz@outlook.co.id>
2026-04-26 14:07:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,56 @@
+// MENU MOBILE
+const toggle = document.getElementById("menuToggle");
+const nav = document.getElementById("navLinks");
+
+toggle.onclick = () => nav.classList.toggle("active");
+
 // ACTIVE NAV
 const sections = document.querySelectorAll("section, header");
-const navLinks = document.querySelectorAll(".nav-links a");
+const links = document.querySelectorAll(".nav-links a");
 
 window.addEventListener("scroll", () => {
   let current = "";
 
-  sections.forEach(section => {
-    if(scrollY >= section.offsetTop - 100){
-      current = section.id;
+  sections.forEach(sec => {
+    if(scrollY >= sec.offsetTop - 100){
+      current = sec.id;
     }
   });
 
-  navLinks.forEach(link => {
+  links.forEach(link => {
     link.classList.remove("active");
     if(link.getAttribute("href") === "#" + current){
       link.classList.add("active");
+    }
+  });
+});
+
+// SKILL ANIMATION
+const skills = document.querySelectorAll(".bar span");
+
+window.addEventListener("scroll", () => {
+  skills.forEach(skill => {
+    const pos = skill.getBoundingClientRect().top;
+    if(pos < window.innerHeight - 100){
+      skill.style.width = skill.getAttribute("style").replace("width:", "");
+    }
+  });
+});
+
+// SCROLL ANIMATION
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(c=>{
+  c.style.opacity=0;
+  c.style.transform="translateY(30px)";
+  c.style.transition="0.6s";
+});
+
+window.addEventListener("scroll", ()=>{
+  cards.forEach(c=>{
+    if(c.getBoundingClientRect().top < window.innerHeight - 100){
+      c.style.opacity=1;
+      c.style.transform="translateY(0)";
     }
   });
 });
