4f945bc75901901f745cd133aaec33f3ed0ddee4
Axel A.L <unknownkz@outlook.co.id>
2026-04-27 06:43:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -50,22 +50,6 @@ document.querySelectorAll(".nav-links a").forEach(link=>{
   });
 });
 
-// ================= SMOOTH SCROLL =================
-document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
-anchor.addEventListener("click", function(e){
-e.preventDefault();
-const target = document.querySelector(this.getAttribute("href"));
-
-if(target){  
-  window.scrollTo({  
-    top: target.offsetTop - 80,  
-    behavior: "smooth"  
-  });  
-}
-
-});
-});
-
 // ================= ACTIVE NAV =================
 const sections = document.querySelectorAll("section, header");
 const links = document.querySelectorAll(".nav-links a");
