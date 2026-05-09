31339977999ba32de987a764338a9f2b61e3ecb0
Axel A.L <unknownkz@outlook.co.id>
2026-05-09 19:34:15 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -38,10 +38,26 @@ self.addEventListener("fetch", event => {
 
   event.respondWith(
 
-    caches.match(event.request)
+    fetch(event.request)
+
       .then(response => {
 
-        return response || fetch(event.request);
+        const responseClone = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(cache => {
+
+            cache.put(event.request, responseClone);
+
+          });
+
+        return response;
+
+      })
+
+      .catch(() => {
+
+        return caches.match(event.request);
 
       })
 
