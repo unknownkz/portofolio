d768e6608ae6b954d657b8a4721d24ec63076e3b
Axel A.L <unknownkz@outlook.co.id>
2026-05-09 23:56:25 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // ================= CACHE VERSION =================
 
-const CACHE_NAME = "axelal-cache-v2026.2.04";
+const CACHE_NAME = "axelal-cache-v2026.2.05";
 
 // ================= FILES =================
 
